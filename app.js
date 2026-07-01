497cfab5361605aab32955d1cfb1859db82ce822
kloodless <kloodless@gmail.com>
2026-07-01 20:08:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("JavaScript berhasil!");
+// Membuat peta
+const map = L.map("map").setView([-6.200000, 106.816666], 13);
+
+// Menambahkan layer OpenStreetMap
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenStreetMap contributors",
+    maxZoom: 19
+}).addTo(map);
+
+// Marker awal
+L.marker([-6.200000, 106.816666])
+    .addTo(map)
+    .bindPopup("📍 ApotekFinder")
+    .openPopup();
